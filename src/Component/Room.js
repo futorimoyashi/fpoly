853990286cf2bfff7239194bc01c7a12b36e4715
futorimoyashi <jamekin196@gmail.com>
2020-06-12 22:13:45 +0900

    add room

--- src/Component/Room.js
+++ src/Component/Room.js
@@ -2,29 +2,53 @@ import React, { useEffect, useState } from 'react'
 // import queryString from 'query-string'
 import axios from 'axios'
 
-import { Table, Row, Col } from 'reactstrap';
+import { Table, Col, Button, Form, FormGroup, Label, Input} from 'reactstrap';
 import {
     Link
 } from "react-router-dom";
 
 export default function Room(props) {
     const [list, setList] = useState('') 
+    const [name, setName] = useState('')
     const id = props.match.params.id
+    const url ='https://quiz-online-fpt.herokuapp.com/rooms/'
+
 
     useEffect(() => {
-        const url ='https://quiz-online-fpt.herokuapp.com/rooms/' + id
-        axios.get(url).then(res => setList(res.data)).catch(error => console.log(error.message))
-    }, [])
+        axios.get(url + id).then(res => setList(res.data)).catch(error => console.log(error.message))
+    }, [name])
+
+    const check = (name) => {
+        axios.post(url, {
+            name: name
+        }).then(res => setName(''))
+    }
 
     return (
-        <Row>
-            <Col>
+        <div>
+            <Col md={{size: 8, offset: 2}}>
+                <Form>
+                    <FormGroup>
+                        <Label for="exampleEmail" className="float-left">Class Code Name</Label>
+                        <Input type="text" name="name" id="exampleEmail" placeholder="class code name" 
+                            value={name} 
+                            onChange={(e) => setName(e.target.value)} />
+                    </FormGroup>
+                    <Button onClick={(e) => {
+                        check(name)
+                        e.preventDefault()
+                    }}>Submit</Button>
+                </Form>
+            </Col>
+            <hr/>
+            <h2 className="text-center">List Class</h2>
+            <Col md={{size: 10, offset: 1}}>
                 <Table>
                     <thead>
                         <tr>
                         <th>ID</th>
                         <th>Name</th>
-                        <th>&nbsp;</th>
+                        <th size={4}>&nbsp;</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -38,6 +62,6 @@ export default function Room(props) {
                     </tbody>
                 </Table>
             </Col>
-        </Row>
+        </div>
     )
 }
